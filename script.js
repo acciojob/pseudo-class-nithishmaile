b8f288bfc585c218dd585ce2bdd8936c7b681afa
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-09-21 15:52:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const div=document.createElement("div");
+const body=document.querySelector("body");
+div.id="container";
+div.textContent="This is the main container element"
+body.appendChild(div)
